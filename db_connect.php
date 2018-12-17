33860ba6fd3e1e73f7067a2ca51fc740c105e324
abidalibaloch <abidbaloch628@gmail.com>
2018-12-17 15:18:41 +0500

    Removed db_var values

--- db_connect.php
+++ db_connect.php
@@ -1,10 +1,6 @@
 <?php
 	//Create required variables
-	$host = "db4free.net";
-	$username = "abidali123";
-	$pasword = "Some123@";
-	$db_name = "abid786todo";
-	$task = "";
+
 
 	$connect = mysqli_connect($host, $username, $pasword, $db_name);
 	
